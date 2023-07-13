15dd7f9560fc5726d6ffeeb674b0a0eff3836593
Balaji GV <balajigv2002@gmail.com>
2023-07-14 01:46:01 +0530

    added final middleware and me route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import Link from 'next/link';
 import { toast } from 'react-hot-toast';
@@ -7,6 +7,7 @@ import { useRouter } from 'next/navigation';
 
 export default function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = useState('nothing');
 
   const logout = async () => {
     try {
@@ -18,11 +19,25 @@ export default function ProfilePage() {
       toast.error(error.message);
     }
   };
+
+  const getUserDetails = async () => {
+    const res = await axios.get('/api/users/me');
+    console.log(res.data.UserData._id);
+    setData(res.data.UserData._id);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <br />
       <p>Profile page</p>
+      <h2 className="p-1 rounded bg-green-500">
+        {data === 'nothing' ? (
+          'Nothing'
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
       <hr />
 
       <button
@@ -30,6 +45,13 @@ export default function ProfilePage() {
         className="bg-blue-500 hover:bg-blue-700 mt-4 text-white font-bold py-2 px-4 rounded"
       >
         Logout
+      </button>
+
+      <button
+        onClick={getUserDetails}
+        className="bg-green-700 hover:bg-blue-700 mt-4 text-white font-bold py-2 px-4 rounded"
+      >
+        GetUser Details
       </button>
     </div>
   );
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,10 @@ import type { NextRequest } from 'next/server';
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
+  console.log(path);
 
   const isPublicPath = path === '/login' || path === '/signup';
+  console.log(isPublicPath);
 
   const token = request.cookies.get('token')?.value || '';
 
